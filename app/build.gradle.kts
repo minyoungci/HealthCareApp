ca4c5a3bbe656040816d300c51a8dc413c96efbd
minyoungci <dbssus123@gmail.com>
2023-11-06 23:12:08 +0900

    design 추

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.minyoungci.healthcareapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.minyoungci.healthcareapp"
